c6d2d1701db48a38ffd0734a55e5e619fa327e8f
alessandrofa <alessandro.f.araujo@gmail.com>
2016-02-17 16:40:41 -0200

    Add frontend form and submit

--- public/js/app.js
+++ public/js/app.js
@@ -7,4 +7,14 @@ socket.on('connect', function () {
 socket.on('message', function(message) {
     console.log('New message:');
     console.log(message.text);
-})
+});
+
+//Handles submitting new messages 
+var $form = jQuery('#message-form');
+
+$form.on('submit', function (event) {
+    event.preventDefault();
+    var $message = $form.find('input[name=message]');
+    socket.emit('message', { text: $message.val() });
+    text: $message.val('');    
+});
